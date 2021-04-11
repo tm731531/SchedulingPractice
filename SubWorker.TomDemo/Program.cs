96493a497ab509f8d033a406b145e91664f9f3a2
ting <tm731531@hotmail.com>
2021-04-11 14:14:00 +0800

    初始專案

---
+++ SubWorker.TomDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SubWorker.TomDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
